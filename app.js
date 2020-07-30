7fd106c09758c8804a1e4dd5467e941cebf7f955
Divindra Ramai <daveramai@hotmail.com>
2020-07-30 19:46:50 -0400

    #10 - connect to mongodb atlas

--- app.js
+++ app.js
@@ -2,12 +2,19 @@
 let express = require("express");
 let app = express();
 let passportSetup = require("./config/passport-setup"); //#2nd stage - kick start/execute the strategy - passport-setup
+let mongoose = require("mongoose");
+let keys = require("./config/keys");
 
 //import router
 let authRoutes = require("./routes/auth-routes");
 
 //set up view engine
 app.set("view engine", "ejs");
+
+//connect to mongodb
+mongoose.connect(keys.mongodb.dbURI, () => {
+  console.log("Connected to mongodb");
+});
 
 //set up routes /auth/...
 app.use("/auth", authRoutes);
