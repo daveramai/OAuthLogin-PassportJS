a13c2f2adb5aabafb3109b550add4214b269ce72
Divindra Ramai <daveramai@hotmail.com>
2020-07-29 22:07:56 -0400

    Google strategy setup and securing keys

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,10 +1,13 @@
 let passport = require("passport");
 let GoogleStrategy = require("passport-google-oauth20");
+let keys = require("./keys");
 
 //tell passport to use google strategy
 passport.use(
   new GoogleStrategy({
     //takes an object for  - options for the google strategy
+    clientID: keys.google.clientID,
+    clientSecret: keys.google.clientSecret,
   }),
   //second param
   () => {
